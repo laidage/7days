7c34e7d84723c9d4bbecd430c95707a4f8e6aa3f
清水 <qingshui@qq.com>
2022-12-28 16:25:17 +0800

    增加一致性哈希

---
+++ Gee/geecache/consistenthash/consistenthash.go
@@ -0,0 +1,50 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type HashFunc func([]byte) uint32
+
+type CHash struct {
+	replicas int
+	hash     HashFunc
+	hashMap  map[int]string
+	keys     []int //sorted
+}
+
+func NewCHash(replicas int, fn HashFunc) *CHash {
+	ch := &CHash{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if ch.hash == nil {
+		ch.hash = crc32.ChecksumIEEE
+	}
+	return ch
+}
+
+func (ch *CHash) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < ch.replicas; i++ {
+			hashValue := int(ch.hash([]byte(strconv.Itoa(i) + key)))
+			ch.keys = append(ch.keys, hashValue)
+			ch.hashMap[hashValue] = key
+		}
+	}
+	sort.Ints(ch.keys)
+}
+
+func (ch *CHash) Get(key string) string {
+	if len(ch.keys) == 0 {
+		return ""
+	}
+	hashValue := int(ch.hash([]byte(key)))
+	idx := sort.Search(len(ch.keys), func(i int) bool {
+		return ch.keys[i] >= hashValue
+	})
+	return ch.hashMap[ch.keys[idx%len(ch.keys)]]
+}
